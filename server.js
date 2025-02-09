22ec67df977ec4c7d74fb53326c9f12b372a57ce
Tobiloba <oluwatobiloba.raji@sanosgroup.co>
2025-02-09 16:24:39 +0100

    feat: remove public folder

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const jsonServer = require("json-server");
 const cors = require("cors");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+  static: "./public"
+});
 
 server.use(cors());
 server.use(middlewares);
 server.use(router);
 
 const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => {
-  console.log(`✅ JSON Server is running on port ${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ JSON Server is running on http://localhost:${PORT}`);
 });
