953ef67c9c214b1173ad29362991447e633187e0
Tobiloba <oluwatobiloba.raji@sanosgroup.co>
2025-02-09 15:55:55 +0100

    feat: setup json-server backend

---
+++ server.js
@@ -0,0 +1,14 @@
+const jsonServer = require("json-server");
+const cors = require("cors");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log(`✅ JSON Server is running on port ${PORT}`);
+});
